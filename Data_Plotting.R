7967a7d44d38aabed0b7fa2dedb6289c2aca7e4b
Aljoscha Goetschi <aljoscha.goetschi@gmx.net>
2023-06-06 12:37:02 +0200

    plot: case vs control

--- Data_Plotting.R
+++ Data_Plotting.R
@@ -1,3 +1,6 @@
 rm(list = ls())
 source("Packages.R")
 source("Data_Cleaning.R")
+
+p_case_cont <- ggplot(dat_overview, aes(x = name, y = value)) + geom_col() +
+  xlab(label = "") + ylab(label = "Number of patients") + theme_bw()
