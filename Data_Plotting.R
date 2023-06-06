2289dde2529bea6a6e0d838b2f8be07c6b8fbb5e
Aljoscha Goetschi <aljoscha.goetschi@gmx.net>
2023-06-06 15:34:57 +0200

    add age plots

--- Data_Plotting.R
+++ Data_Plotting.R
@@ -20,5 +20,11 @@ p_hist_alc_case <- ggplot(dat, aes(x = alcohol, y = cases)) + geom_col() + xlab(
 p_hist_alc_control <- ggplot(dat, aes(x = alcohol, y = controls)) + geom_col() + xlab(label = "Alcohol consumption [g/day]") +
   ylab("Number of controls") + theme_bw()
 
-p_grid_hist <- plot_grid(plotlist = list(p_hist_smoke_case, p_hist_smoke_control, p_hist_alc_case, p_hist_alc_control), 
-                         labels = c("V1", "V2", "V3", "V4"), label_size = 12, nrow = 2, hjust = -0.25)
+p_hist_age_case <- ggplot(dat, aes(x = age, y = cases)) + geom_col() + xlab(label = "Age groups [years]") +
+  ylab("Number of cases") + theme_bw()
+
+p_hist_age_control <- ggplot(dat, aes(x = age, y = controls)) + geom_col() + xlab(label = "Age groups [years]") +
+  ylab("Number of cases") + theme_bw()
+
+p_grid_hist <- plot_grid(plotlist = list(p_hist_smoke_case, p_hist_smoke_control, p_hist_alc_case, p_hist_alc_control, p_hist_age_case, p_hist_age_control), 
+                         labels = c("V1", "V2", "V3", "V4", "V5", "V6"), label_size = 10, nrow = 3, hjust = -0.25, vjust = 1)
