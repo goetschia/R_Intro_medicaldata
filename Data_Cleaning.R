16c675a668d2256a35a937be63ed5e10ac94032a
Aljoscha Goetschi <aljoscha.goetschi@gmx.net>
2023-06-06 12:22:59 +0200

    Wrote script

---
+++ Data_Cleaning.R
@@ -0,0 +1,12 @@
+rm(list = ls())
+source("Packages.R")
+
+data("esoph_ca")
+
+dat <- esoph_ca
+names(dat) <- c("age", "alcohol", "smoker", "cases", "controls")
+
+dat <- dat %>% mutate(alcohol = if_else(alcohol == "0-39g/day", "0-39", alcohol),
+                      smoker = if_else(smoker == "0-9g/day", "0-9", smoker))
+
+rm(esoph_ca)
