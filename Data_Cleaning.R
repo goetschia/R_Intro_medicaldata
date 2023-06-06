3adab96ba3dfe9ada9eea9876f8b10ba7a4f76ff
Aljoscha Goetschi <aljoscha.goetschi@gmx.net>
2023-06-06 12:35:56 +0200

    Added comparison of cases vs controls

--- Data_Cleaning.R
+++ Data_Cleaning.R
@@ -9,4 +9,6 @@ names(dat) <- c("age", "alcohol", "smoker", "cases", "controls")
 dat <- dat %>% mutate(alcohol = if_else(alcohol == "0-39g/day", "0-39", alcohol),
                       smoker = if_else(smoker == "0-9g/day", "0-9", smoker))
 
+dat_overview <- dat %>% summarise(cases = sum(cases), controls = sum(controls)) %>% pivot_longer(c(cases,controls))
+
 rm(esoph_ca)
